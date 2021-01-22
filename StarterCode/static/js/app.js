a8345ef74b38b1860a0c6a62e41f4cf61d8bead5
Jeawills <jeawills2@gmail.com>
2021-01-21 23:39:38 -0500

    adding files

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,52 @@
+// from data.js
+var tableData = data;
+console.log(data);
+
+// Identify the table and tbody
+var tbody = d3.select('#ufo-table>tbody');
+
+// Create function to generate and populate the table
+function buildTable(tableData){
+    // Dynamically build table
+    tableData.forEach(record => {
+        var row = tbody.append('tr');
+
+         Object.values(record).forEach(col => {
+            row.append('td').text(col);        
+        });
+    })
+}
+
+function filterTable(elem){
+    console.log('filter table event');
+        
+    //var changedElem = d3.event.target;
+    var dateElem = d3.select('#datetime');   
+    var filterDate = dateElem.property('value'); 
+    
+    filteredData = tableData.filter(rec => rec['datetime'] == filterDate);
+
+    console.log(filteredData);
+
+    // Clear out the tbody
+    tbody.html('');
+
+    // Rebuild the filtered table using the buildTable function 
+    buildTable(filteredData);
+}
+
+// Identify web elements on the page
+btn = d3.select('#filter-btn');
+datetimefield = d3.select('#datetime')
+
+// Add event listeners to the web elements
+btn.on('click', filterTable);
+datetimefield.on('change', filterTable);
+
+// Call the function to initially load the table
+buildTable(tableData);
+
+
+
+
+
